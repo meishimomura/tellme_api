4219b13a49bb313212b0d8dec69dc077f7d6ca46
下村芽生 <mei.shimomura.hal@gmail.com>
2022-01-27 12:45:36 +0900

    notificationモデルのhas_manyを修正

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :user, foreign_key: :uid
   belongs_to :group
-  has_many :notifications, dependent: :destroy
+  has_many :notification_checks, dependent: :destroy
 end
