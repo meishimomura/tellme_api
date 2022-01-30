5942f3f60c0fd4a3a8a6ac186ced8545f1e2c927
下村芽生 <mei.shimomura.hal@gmail.com>
2022-01-30 17:32:12 +0900

    json メッセージ修正

--- app/controllers/api/v1/student/subjects_controller.rb
+++ app/controllers/api/v1/student/subjects_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::Student::SubjectsController < ApplicationController
 
   def index
     subjects = Subject.where(id: GroupDirector.where(group_id: current_api_v1_user.group_id).select("group_directors.subject_id"))
-    render json: { status: 'SUCCESS', message: 'Loaded the school', data: subjects }
+    render json: { status: 'SUCCESS', message: 'Loaded the subjects', data: subjects }
   end
 end
