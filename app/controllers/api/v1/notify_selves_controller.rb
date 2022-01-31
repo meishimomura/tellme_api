47b0e5c081ba636a7ddfe8e2e9f74254c02887a4
下村芽生 <mei.shimomura.hal@gmail.com>
2022-01-31 23:52:45 +0900

    コメントアウトを外す

--- app/controllers/api/v1/notify_selves_controller.rb
+++ app/controllers/api/v1/notify_selves_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::NotifySelvesController < ApplicationController
-  # before_action :authenticate_api_v1_user!
+  before_action :authenticate_api_v1_user!
   before_action :set_notify_self, only: [:show, :update, :destroy]
 
   def index
