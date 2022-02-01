0902b90789d2a7a154c7c63d4e5f673fdaffe875
下村芽生 <mei.shimomura.hal@gmail.com>
2022-02-01 11:39:47 +0900

    所属するグループの科目を生徒と教師で条件分岐して返すAPI作成

--- app/controllers/api/v1/student/subjects_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::Student::SubjectsController < ApplicationController
-  before_action :authenticate_api_v1_user!
-
-  def index
-    subjects = Subject.where(id: GroupDirector.where(group_id: current_api_v1_user.group_id).select("group_directors.subject_id"))
-    render json: { status: 'SUCCESS', message: 'Loaded the subjects', data: subjects }
-  end
-end
