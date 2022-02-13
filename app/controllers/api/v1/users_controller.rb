16b09d72e9a928c7d5fd006d0f5e0c056ce6a20f
下村芽生 <mei.shimomura.hal@gmail.com>
2022-02-13 17:44:53 +0900

    userデータを返すAPIに生徒と教師の条件分岐を追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::UsersController < ApplicationController
   before_action :authenticate_api_v1_user!
 
   def index
-    user = User.joins(:school).joins(:group).select("users.*, schools.school_name, groups.group_grade, groups.group_name").find_by(uid: current_api_v1_user.uid)
+    user = User.find_by(uid: current_api_v1_user.uid)
+    if user.user_is_student
+      user = User.joins(:school).joins(:group).select("users.*, schools.school_name, groups.group_grade, groups.group_name").find_by(uid: current_api_v1_user.uid)
+    else
+      user = User.joins(:school).select("users.*, schools.school_name").find_by(uid: current_api_v1_user.uid)
+    end
     render json: { status: 'SUCCESS', message: 'Loaded user', data: user }
   end
 end
